ec3a236fa4711cfe9ae128834496ec4596d92de5
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2023-08-20 13:02:00 +0100

    initial logo and text placement

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>Index page</h1>
+  <div class="w-screen flex justify-between">
+    <div class="bg-white w-2/3 mt-20">
+      <img class="w-full" src="~/assets/images/teiai-logo-b-transparent.png" />
+    </div>
+    <div class="flex flex-col justify-center items-end mr-4 gap-2 z-10 w-1/3">
+      <span class="text-2xl sm:text-4xl md:text-5xl transition-all"
+        >SECURING</span
+      >
+      <span class="text-2xl sm:text-4xl md:text-5xl transition-all">YOUR</span>
+      <span
+        class="text-2xl sm:text-4xl md:text-5xl transition-all text-yellow-500"
+        >FUTURE</span
+      >
+    </div>
+  </div>
 </template>
