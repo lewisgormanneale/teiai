b4c8a8bd76276583eefa21ae8f0dcfc132adc802
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2023-08-20 10:53:00 +0100

    google fonts, header, main css file

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
+  <div class="flex flex-col w-screen items-center">
+    <h1 class="text-2xl text-center py-2 font-bold">TEIAI GROUP</h1>
+    <div class="w-64 h-px bg-white mb-2"></div>
     <NuxtPage />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 export default defineNuxtConfig({
+  css: ["@/assets/css/main.css"],
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
+  googleFonts: {
+    families: {
+      "Libre Baskerville": true,
+    },
+  },
 });
