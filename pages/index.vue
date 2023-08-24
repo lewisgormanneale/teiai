6213e4721d1c2d32d8541a58eef32537bfcd0294
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2023-08-24 09:36:42 +0100

    better image width, paragraph content

--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,13 @@
   <div class="w-full flex justify-between overflow-y-visible max-h-screen">
     <div class="w-full mt-10" style="margin-left: -120px">
       <nuxt-img
-        class="z-20"
+        class="z-20 w-full"
         src="teiai-logo-b-transparent.svg"
-        style="background-image: url(sea.gif); background-size: cover"
+        style="background-image: url(mountains.gif); background-size: cover"
       />
     </div>
     <div
-      class="flex flex-col justify-evenly items-end gap-2 z-10 w-1/3 h-96 transition-all"
+      class="flex flex-col justify-evenly items-end gap-4 z-10 w-1/3 mr-1 sm:mr-5 transition-all"
     >
       <div class="flex flex-col justify-center items-end gap-2 w-full pr-4">
         <span
@@ -33,25 +33,29 @@
     </div>
   </div>
   <div class="w-full flex flex-col items-end z-10">
-    <div class="flex flex-col items-end gap-2">
+    <div class="flex flex-col items-end gap-2 mr-1 sm:mr-5">
       <span class="text-2xl sm:text-4xl md:text-5xl transition-all">ABOUT</span>
       <span class="text-2xl sm:text-4xl md:text-5xl transition-all"
         >TEIAI GROUP</span
       >
       <div class="w-96 h-px bg-white mb-6"></div>
     </div>
-    <div class="w-full md:w-1/3 transition-all">
-      <p class="text-right">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate enim
-        atque consectetur similique explicabo ea ullam amet unde voluptatibus
-        cumque quas voluptatem vitae totam odio aliquid, sequi, corporis soluta
-        reprehenderit!
+    <div class="w-full sm:w-2/3 lg:w-1/3 transition-all mb-5 mr-1 sm:mr-5">
+      <p class="text-right mb-5">
+        Teiai Group a dynamic financial consultancy dedicated to helping clients
+        navigate the complexities of modern finance. Founded with a commitment
+        to excellence, we offer a range of services that include financial
+        advice, innovative lending solutions, and exclusive events for our
+        esteemed clientele. Under the visionary leadership of President Kazutaka
+        Hyōdō, Teiai has evolved into a multifaceted entity that specializes in
+        various financial endeavors.
       </p>
       <p class="text-right">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate enim
-        atque consectetur similique explicabo ea ullam amet unde voluptatibus
-        cumque quas voluptatem vitae totam odio aliquid, sequi, corporis soluta
-        reprehenderit!
+        At Teiai, we value discretion and professionalism, and aim to offer
+        solutions that maximize our clients' financial potential. Explore our
+        website to learn more about Teiai Corporation and how we can assist you
+        in achieving your financial goals. Join us in experiencing the
+        dedication and excellence that define Teiai's approach to finance.
       </p>
     </div>
 
