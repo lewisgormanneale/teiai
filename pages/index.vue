db8da8c899d9fe856d04228b1771650e935f0e6d
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2023-08-24 17:40:48 +0100

    description tweaks

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="w-full flex justify-between overflow-y-visible max-h-screen">
-    <div class="w-full mt-10" style="margin-left: -120px">
+    <div class="w-full  mt-10" style="margin-left: -120px">
       <nuxt-img
-        class="z-20 w-full"
+        class="z-20 w-full bg-yellow-500"
         src="teiai-logo-b-transparent.svg"
-        style="background-image: url(mountains.gif); background-size: cover"
       />
     </div>
     <div
-      class="flex flex-col justify-evenly items-end gap-4 z-10 w-1/3 mr-1 sm:mr-5 transition-all"
+      class="flex flex-col justify-evenly items-end gap-4 z-10 w-1/3 px-2 sm:pr-5 transition-all"
     >
       <div class="flex flex-col justify-center items-end gap-2 w-full pr-4">
         <span
@@ -32,49 +31,50 @@
       </div>
     </div>
   </div>
-  <div class="w-full flex flex-col items-end z-10">
-    <div class="flex flex-col items-end gap-2 mr-1 sm:mr-5">
+  <div class="w-full flex flex-col items-end z-10 px-2 sm:pr-5">
+    <div class="flex flex-col items-end gap-2 px-2">
       <span class="text-2xl sm:text-4xl md:text-5xl transition-all">ABOUT</span>
       <span class="text-2xl sm:text-4xl md:text-5xl transition-all"
         >TEIAI GROUP</span
       >
       <div class="w-96 h-px bg-white mb-6"></div>
     </div>
-    <div class="w-full sm:w-2/3 lg:w-1/3 transition-all mb-5 mr-1 sm:mr-5">
+    <div class="w-full sm:w-2/3 lg:w-1/3 transition-all mb-5">
       <p class="text-right mb-5">
-        Teiai Group a dynamic financial consultancy dedicated to helping clients
-        navigate the complexities of modern finance. Founded with a commitment
-        to excellence, we offer a range of services that include financial
-        advice, innovative lending solutions, and exclusive events for our
-        esteemed clientele. Under the visionary leadership of President Kazutaka
-        Hyōdō, Teiai has evolved into a multifaceted entity that specializes in
-        various financial endeavors.
+        Teiai Group - a dynamic financial consultancy dedicated to helping
+        clients navigate the complexities of modern finance. Under the visionary
+        leadership of President Kazutaka Hyōdō, Teiai Group has evolved to offer
+        a range of services that include financial advice, innovative lending
+        solutions, and exclusive events for our esteemed clientele.
       </p>
       <p class="text-right">
         At Teiai, we value discretion and professionalism, and aim to offer
-        solutions that maximize our clients' financial potential. Explore our
-        website to learn more about Teiai Corporation and how we can assist you
-        in achieving your financial goals. Join us in experiencing the
-        dedication and excellence that define Teiai's approach to finance.
+        solutions that maximize our clients' financial potential. Join us in
+        experiencing the dedication and excellence that define Teiai's approach
+        to finance.
       </p>
     </div>
 
-    <div class="flex flex-col items-end gap-2">
+    <div class="flex flex-col items-end gap-2 mb-5">
       <span class="text-2xl sm:text-4xl md:text-5xl transition-all"
         >SOLUTIONS</span
       >
       <div class="w-96 h-px bg-white mb-6"></div>
-      <nuxt-img src="restricted-rps.jpeg" format="webp"></nuxt-img>
+      <div class="flex w-full">
+        <nuxt-img src="restricted-rps.jpeg" format="webp"></nuxt-img>
+        <nuxt-img src="restricted-rps.jpeg" format="webp"></nuxt-img>
+        <nuxt-img src="restricted-rps.jpeg" format="webp"></nuxt-img>
+      </div>
     </div>
 
-    <div class="flex flex-col items-end gap-2">
+    <div class="flex flex-col items-end gap-2 mb-5">
       <span class="text-2xl sm:text-4xl md:text-5xl transition-all"
         >WHO WE ARE</span
       >
       <div class="w-96 h-px bg-white mb-6"></div>
       <nuxt-img src="teiai-banner.jpg" format="webp"></nuxt-img>
     </div>
-    <div class="flex flex-col items-end gap-2">
+    <div class="flex flex-col items-end gap-2 mb-5">
       <span class="text-2xl sm:text-4xl md:text-5xl transition-all"
         >NEWSROOM</span
       >
