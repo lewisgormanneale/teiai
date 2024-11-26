02c2cc96240f973ff6e2dbc3eb110c6060517768
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2024-11-26 13:05:40 +0000

    install shadcn-nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,42 +1,55 @@
 export default defineNuxtConfig({
-  app: {
-    head: {
-      charset: "utf-8",
-      htmlAttrs: {
-        lang: "en",
-      },
-      link: [],
-      meta: [
-        {
-          hid: "description",
-          name: "description",
-          content:
-            "Teiai Group - a dynamic financial consultancy dedicated to helping clients navigate the complexities of modern finance. Find out more information here.",
+    app: {
+        head: {
+            charset: "utf-8",
+            htmlAttrs: {
+                lang: "en",
+            },
+            link: [],
+            meta: [
+                {
+                    hid: "description",
+                    name: "description",
+                    content:
+                        "Teiai Group - a dynamic financial consultancy dedicated to helping clients navigate the complexities of modern finance. Find out more information here.",
+                },
+            ],
+            title: "Teiai Group",
+            viewport: "width=device-width, initial-scale=1",
         },
-      ],
-      title: "Teiai Group",
-      viewport: "width=device-width, initial-scale=1",
     },
-  },
-  css: ["@/assets/css/main.css"],
-  devtools: { enabled: true },
-  image: {
-    dir: "assets/images",
-    inject: true,
-  },
-  googleFonts: {
-    families: {
-      "Libre Baskerville": true,
-      Montserrat: true,
+    css: ["@/assets/css/main.css"],
+    devtools: {enabled: true},
+    image: {
+        dir: "assets/images",
+        inject: true,
     },
-  },
-  nitro: {
-    compressPublicAssets: true,
-  },
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "@nuxtjs/google-fonts",
-    "nuxt-icon",
-    "@nuxt/image",
-  ],
+    googleFonts: {
+        families: {
+            "Libre Baskerville": true,
+            Montserrat: true,
+        },
+    },
+    nitro: {
+        compressPublicAssets: true,
+    },
+    modules: [
+        "@nuxtjs/tailwindcss",
+        "@nuxtjs/google-fonts",
+        "@nuxtjs/color-mode",
+        "shadcn-nuxt",
+        "nuxt-icon",
+        "@nuxt/image",
+    ],
+    shadcn: {
+        /**
+         * Prefix for all the imported component
+         */
+        prefix: '',
+        /**
+         * Directory that the component lives in.
+         * @default "./components/ui"
+         */
+        componentDir: './components/ui'
+    }
 });
